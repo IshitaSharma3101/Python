b579b0fff9b457b7abc64d937693fffec1633669
IshitaSharma3101 <72185572+IshitaSharma3101@users.noreply.github.com>
2021-07-01 21:00:09 +0530

    chapter 4 ques 2

---
+++ chapter_04/ques2.py
@@ -0,0 +1,11 @@
+marks1 = int(input("Marks1 : "))
+marks2 = int(input("Marks2 : "))
+marks3 = int(input("Marks3 : "))
+marks4 = int(input("Marks4 : "))
+marks5 = int(input("Marks5 : "))
+marks6 = int(input("Marks6 : "))
+
+marks = [marks1, marks2, marks3, marks4, marks5, marks6]
+
+marks.sort()
+print(marks)
