6b6351ce74ebd51aec6afc2d02e31b8a620d6b81
IshitaSharma3101 <72185572+IshitaSharma3101@users.noreply.github.com>
2021-07-01 20:55:00 +0530

    chapter 4 ques 1

---
+++ chapter_04/ques1.py
@@ -0,0 +1,10 @@
+fruit1 = input("Enter fruit1 : ")
+fruit2 = input("Enter fruit2 : ")
+fruit3 = input("Enter fruit3 : ")
+fruit4 = input("Enter fruit4 : ")
+fruit5 = input("Enter fruit5 : ")
+fruit6 = input("Enter fruit6 : ")
+fruit7 = input("Enter fruit7 : ")
+
+fruitsList = [fruit1, fruit2, fruit3, fruit4, fruit5, fruit6, fruit7]
+print(fruitsList)
