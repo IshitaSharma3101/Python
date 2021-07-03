2c31085cbc92f3ec443dff1b241667d15129bdd3
IshitaSharma3101 <72185572+IshitaSharma3101@users.noreply.github.com>
2021-07-03 13:07:57 +0530

    chapter 5 ques 7

---
+++ chapter_05/ques7.py
@@ -0,0 +1,16 @@
+d = {}
+jhalak = input("Enter a language jhalak : ")
+hardik = input("Enter a language hardik : ")
+shrishti = input("Enter a language shrishti : ")
+hardik = input("Enter a language hardik : ")
+
+newD = {
+    "jhalak":jhalak,
+    "hardik":hardik,
+    "shrishti":shrishti,
+    "hardik":hardik
+}
+d.update(newD)
+
+
+print(d)
