570e908e14de9a78e5c4d39ad04cbcee29834484
IshitaSharma3101 <72185572+IshitaSharma3101@users.noreply.github.com>
2021-07-03 12:49:29 +0530

    chapter 5 ques 2

---
+++ chapter_05/ques2.py
@@ -0,0 +1,12 @@
+no1 = int(input("No 1 : "))
+no2 = int(input("No 2 : "))
+no3 = int(input("No 3 : "))
+no4 = int(input("No 4 : "))
+no5 = int(input("No 5 : "))
+no6 = int(input("No 6 : "))
+no7 = int(input("No 7 : "))
+no8 = int(input("No 8 : "))
+
+s = {no1, no2, no3, no4, no5, no6, no7, no8}
+
+print(s)
