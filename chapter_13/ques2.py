93b89502d1539113e5267eef69198ae897e89700
IshitaSharma3101 <72185572+IshitaSharma3101@users.noreply.github.com>
2021-08-07 01:15:44 +0530

    chapter 13 ques 2

---
+++ chapter_13/ques2.py
@@ -0,0 +1,6 @@
+name = input("Enter name : ")
+marks = input("Enter marks : ")
+phone = input("Enter phone : ")
+
+template = "Student is {} whose marks are {} and phone number is {}".format(name, marks, phone)
+print(template)
